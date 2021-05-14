a48447d42fc55ae0a762d286afd4092d734f6175
Bob Saska <git@r35.net>
2021-05-14 01:42:18 -0500

    chore: removing the echo prefix

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,7 +9,7 @@ install_file() {
     # $3 = mode (0600)
     local target_mode=${3:-0600}
     step_line "Installing" "${1} --> ${2}"
-    echo install -m ${target_mode} files/${1} ${2}
+    install -m ${target_mode} files/${1} ${2}
 }
 
 # Install Files
