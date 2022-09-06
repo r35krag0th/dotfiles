d0acb0eff427af0410948260c5f675d9464e0c1b
Bob Saska <git@r35.net>
2022-09-06 10:39:17 -0500

    chore: fixing pyenv vs homebrew path

--- dot_zshrc.d/001-binfiles.zsh
+++ dot_zshrc.d/001-binfiles.zsh
@@ -6,5 +6,6 @@ if [ -d "$HOME/.local/bin" ]; then
     export PATH="${HOME}/.local/bin:$PATH";
 fi
 
-[ -f $HOME/workspace/personal/dotfiles-v2/lib/onelogin_shim.zsh ] \
-  && . ${0:A:h}/../../lib/onelogin_shim.zsh
+onelogin_shim_path="$HOME/workspace/personal/dotfiles-v2/lib/onelogin_shim.zsh"
+[ -f "${onelogin_shim_path}" ] \
+  && . ${onelogin_shim_path}
