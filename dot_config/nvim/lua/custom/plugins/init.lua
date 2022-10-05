09e0a0c22c12e78b204b0c99803d52ada0c31192
Bob Saska <git@r35.net>
2022-10-05 13:15:30 -0500

    chore: adding ray-x/go.nvim

--- dot_config/nvim/lua/custom/plugins/init.lua
+++ dot_config/nvim/lua/custom/plugins/init.lua
@@ -39,4 +39,11 @@ return {
       require "custom.plugins.null-ls"
     end,
   },
+
+  ["ray-x/go.nvim"] = {
+    requires = { "ray-x/guihua.lua" },
+    config = function()
+      require("go").setup()
+    end,
+  },
 }
