f01158fc9a695ad47f75da5c00d6cb47f0e7a19c
Bob Saska <git@r35.net>
2025-12-12 23:29:19 -0600

    chore: use mise to find assume.fish

--- dot_config/private_fish/functions/private_assume.fish
+++ dot_config/private_fish/functions/private_assume.fish
@@ -1,10 +1,13 @@
 # AWS assume role helper (granted.dev)
 # Sources the assume.fish script from Homebrew
 function assume -d "Assume an AWS role using granted"
-    if set -q R35_HOMEBREW_BIN; and test -f "$R35_HOMEBREW_BIN/assume.fish"
-        source $R35_HOMEBREW_BIN/assume.fish $argv
+    set -l mise_granted_root (mise where granted)
+    if test -d $mise_granted_root; and test -f $mise_granted_root/assume.fish
+        source "$mise_granted_root/assume.fish" $argv
+    else if set -q R35_HOMEBREW_BIN; and test -f "$R35_HOMEBREW_BIN/assume.fish"
+        source "$R35_HOMEBREW_BIN/assume.fish" $argv
     else if test -f /opt/homebrew/bin/assume.fish
-        source /opt/homebrew/bin/assume.fish $argv
+        source "/opt/homebrew/bin/assume.fish" $argv
     else
         echo "assume.fish not found - install granted: brew install common-fate/granted/granted" >&2
         return 1
