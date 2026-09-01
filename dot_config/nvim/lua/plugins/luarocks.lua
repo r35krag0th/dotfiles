012af6f333389f480b71663e8ccbe0e640627df9
Bob Saska <git@r35.net>
2026-09-01 16:55:28 -0500

    chore: update mise conf

--- dot_config/nvim/lua/plugins/luarocks.lua
+++ dot_config/nvim/lua/plugins/luarocks.lua
@@ -1,9 +1,17 @@
 return {
   "vhyrro/luarocks.nvim",
-  priority = 1000,
+  priority = 1001,
+  enabled = true,
   config = true,
+  dir = "~/workspace/luarocks.nvim",
   opts = {
     -- This can get a little sussy when there are MITM surprises
-    rocks = { "dkjson", "lua-curl", "nvim-nio", "mimetypes", "xml2lua" },
+    rocks = {
+      "dkjson",
+      "lua-curl",
+      -- "nvim-nio",
+      "mimetypes",
+      "xml2lua",
+    },
   },
 }
